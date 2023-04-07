9b66dff69ef6440f5d68b6d2e86d80db3a9d21f1
shoxa-mir <shoxamiraliyev@gmail.com>
2023-04-07 20:29:05 +0900

    Changed openai_model

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,11 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.grey,
+        // colorScheme: ColorScheme.fromSwatch().copyWith(
+        //     primary: Color.fromARGB(255, 114, 169, 163),
+        //     secondary: Colors.amber),
+        primarySwatch: Colors.deepPurple,
+        primaryColor: Colors.deepPurpleAccent,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
@@ -39,12 +43,26 @@ class _MyHomePageState extends State<MyHomePage> {
   ChatUser openAiUser =
       ChatUser(id: "2", firstName: "CHATGPT", lastName: "OpenAi");
 
+  void _chatGpt3Example(String textInput) async {
+    final request = ChatCompleteText(messages: [
+      Map.of({"role": "user", "content": "${textInput}"})
+    ], maxToken: 200, model: ChatModel.ChatGptTurbo0301Model);
+
+    final response = await openAI.onChatCompletion(request: request);
+    for (var element in response!.choices) {
+      print("data -> ${element.message.content}");
+    }
+    setState(() {
+      results = response.choices.first.message.content;
+    });
+  }
+
   @override
   void initState() {
     super.initState();
     openAI = OpenAI.instance.build(
-        token: "open-ai-api-key",
-        baseOption: HttpSetup(receiveTimeout: const Duration(seconds: 5)),
+        token: "openai-api-key",
+        baseOption: HttpSetup(receiveTimeout: const Duration(seconds: 15)),
         isLog: true);
   }
 
@@ -55,87 +73,108 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text("Open AI Chatbot "),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: <Widget>[
-            Expanded(
-              child: DashChat(
-                currentUser: user,
-                onSend: (ChatMessage m) {
-                  setState(() {
-                    messages.insert(0, m);
-                  });
-                },
-                messages: messages,
-                readOnly: true,
-              ),
-            ),
-            Row(
-              children: [
-                Expanded(
-                    child: Card(
-                        color: Colors.grey[100],
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(10)),
-                        child: Padding(
-                          padding: const EdgeInsets.symmetric(
-                              vertical: 8.0, horizontal: 16.0),
-                          child: TextField(
-                            controller: textEditingController,
-                            decoration: InputDecoration(
-                                border: InputBorder.none,
-                                hintText: "Type your message here..."),
-                          ),
-                        ))),
-                ElevatedButton(
-                  onPressed: () async {
-                    ChatMessage msg = ChatMessage(
-                        text: textEditingController.text,
-                        user: user,
-                        createdAt: DateTime.now());
+        child: Container(
+          padding: EdgeInsets.symmetric(vertical: 10),
+          color: Colors.white,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              Expanded(
+                child: DashChat(
+                  currentUser: user,
+                  onSend: (ChatMessage m) {
                     setState(() {
-                      messages.insert(0, msg);
+                      messages.insert(0, m);
                     });
-
-                    final request = CompleteText(
-                        prompt: textEditingController.text,
-                        model: Model.TextDavinci3,
-                        maxTokens: 200);
-                    // final request = CompleteText(
-                    //     prompt: translateToJapanese(
-                    //         word: textEditingController.text),
-                    //     model: Model.TextDavinci3,
-                    //     maxTokens: 200);
-                    final response =
-                        await openAI.onCompletion(request: request);
-                    ChatMessage msg2 = ChatMessage(
-                        user: openAiUser,
-                        text: response!.choices.first.text,
-                        createdAt: DateTime.now());
-                    // results = response!.choices.first.text;
-                    setState(() {
-                      messages.insert(0, msg2);
-                      // results;
-                    });
-                    // final request = GenerateImage(
-                    //     textEditingController.text, 1,
-                    //     size: ImageSize.size512, responseFormat: Format.url);
-                    // ;
-
-                    // final res = openAI.generateImage(request);
-                    // res.then((value) =>
-                    //     print("img url :${value!.data!.first!.url}"));
-                    textEditingController.clear();
                   },
-                  child: Icon(Icons.send),
-                  style: ElevatedButton.styleFrom(
-                      shape: CircleBorder(),
-                      padding: EdgeInsets.all(12),
-                      backgroundColor: Colors.white),
-                )
-              ],
-            )
-          ],
+                  messages: messages,
+                  readOnly: true,
+                ),
+              ),
+              Row(
+                children: [
+                  Expanded(
+                      child: Card(
+                          color: Colors.grey[200],
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10)),
+                          child: Padding(
+                            padding: const EdgeInsets.symmetric(
+                                vertical: 8.0, horizontal: 16.0),
+                            child: TextField(
+                              controller: textEditingController,
+                              decoration: InputDecoration(
+                                  border: InputBorder.none,
+                                  hintText: "Type your message here..."),
+                            ),
+                          ))),
+                  ElevatedButton(
+                    onPressed: () async {
+                      ChatMessage msg = ChatMessage(
+                          text: textEditingController.text,
+                          user: user,
+                          createdAt: DateTime.now());
+                      setState(() {
+                        messages.insert(0, msg);
+                      });
+
+                      // final request = ChatCompleteText(messages: [
+                      //   Map.of({"role": "user", "content": 'Hello!'})
+                      // ], maxToken: 200, model: ChatModel.ChatGptTurbo0301Model);
+
+                      // final response =
+                      //     await openAI.onChatCompletion(request: request);
+                      // for (var element in response!.choices) {
+                      //   print("data -> ${element.message.content}");
+                      // }
+                      // final request = CompleteText(
+                      //     prompt: textEditingController.text,
+                      //     model: Model.values.first,
+                      //     maxTokens: 50);
+                      // final models = await openAI.listEngine();
+                      // print(models.data);
+                      _chatGpt3Example(textEditingController.text);
+                      textEditingController.clear();
+                      // final request = CompleteText(
+                      //     prompt: translateToJapanese(
+                      //         word: textEditingController.text),
+                      //     model: Model.TextDavinci3,
+                      //     maxTokens: 200);
+                      // final response =
+                      //     await openAI.onCompletion(request: request);
+                      ChatMessage msg2 = ChatMessage(
+                          replyTo: msg,
+                          user: openAiUser,
+                          text: results,
+                          quickReplies: [],
+                          createdAt: DateTime.now());
+                      // results = response!.choices.first.text;
+                      setState(() {
+                        messages.insert(0, msg2);
+                        // results;
+                      });
+                      // final request = GenerateImage(
+                      //     textEditingController.text, 1,
+                      //     size: ImageSize.size512, responseFormat: Format.url);
+                      // ;
+
+                      // final res = openAI.generateImage(request);
+                      // res.then((value) =>
+                      //     print("img url :${value!.data!.first!.url}"));
+                    },
+                    child: Icon(
+                      Icons.send,
+                      color: Colors.deepPurple,
+                    ),
+                    style: ElevatedButton.styleFrom(
+                        shape: CircleBorder(),
+                        padding: EdgeInsets.all(12),
+                        backgroundColor: Colors.white),
+                  )
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
